41cb5e2c490ea7888358c131ccfe92ecb0621b34
upskillingcoding-Ritik <upskillingcoding@gmail.com>
2025-09-09 22:35:04 +0530

    basic structure create

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,12 @@
-// import './App.css'
-
-// function App() {
-//     return (
-//     <>
-//       <h1>NPM RUN DEV</h1>
-//     </>
-//   )
-// }
-
-// export default App
-import Sidebar from './components/Sidebar';
-import Dashboard from './components/Dashboard';
+import './App.css'
 
 function App() {
-  return (
-    <div className="flex min-h-screen bg-gray-100">
-      <Sidebar />
-      <Dashboard />
-    </div>
-  );
+    return (
+    <>
+      <h1>NPM RUN DEV</h1>
+    </>
+  )
 }
 
-export default App;
+export default App
+
